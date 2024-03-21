68f3dbb26643da6713be254d528c85ffcc912dab
SEONGEUN0415 <wltnr1998@gamil.com>
2024-03-21 17:50:31 +0900

    animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 $(function () {
-  //header
-  //swiching toggle
-  
-  //mainvisual typing
+    //mainvisual typing
 
     var typingBool = false;
     var typingIdx = 0;
@@ -44,5 +41,72 @@ $(function () {
                 tyInt = setInterval(typing, 100);
             }, 1000);
         }
-    }
+    } //mainvisual typing end
+
+    //skill icon click
+    let skillsDes = document.querySelectorAll(".skills__des p");
+    let skillIcon = document.querySelectorAll(".skills__des--icon li");
+    for (let i = 0; i < skillIcon.length; i++) {
+        skillIcon[i].addEventListener("click", () => {
+            skillsDes.forEach((item) => {
+                item.classList.remove("on");
+            });
+            skillsDes[i].classList.add("on");
+        });
+    } //icon click end
+
+    //about
+    //showing img
+    anime
+        .timeline({ loop: true })
+        .add({
+            targets: ".ml8 .circle-white",
+            scale: [0, 3],
+            opacity: [1, 0],
+            easing: "easeInOutExpo",
+            rotateZ: 360,
+            duration: 1100,
+        })
+        .add({
+            targets: ".ml8 .circle-container",
+            scale: [0, 1],
+            duration: 1100,
+            easing: "easeInOutExpo",
+            offset: "-=1000",
+        })
+        .add({
+            targets: ".ml8 .circle-dark",
+            scale: [0, 1],
+            duration: 1100,
+            easing: "easeOutExpo",
+            offset: "-=600",
+        })
+        .add({
+            targets: ".ml8 .letters-left",
+            scale: [0, 1],
+            duration: 1200,
+            offset: "-=550",
+        })
+        .add({
+            targets: ".ml8 .bang",
+            scale: [0, 1],
+            rotateZ: [45, 15],
+            duration: 1200,
+            offset: "-=1000",
+        })
+        .add({
+            targets: ".ml8",
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1400,
+        });
+
+    anime({
+        targets: ".ml8 .circle-dark-dashed",
+        rotateZ: 360,
+        duration: 8000,
+        easing: "linear",
+        loop: true,
+    });
 });
