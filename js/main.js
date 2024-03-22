f58407c2ae3bb635bb500e3d71456a0bf00ddb61
SEONGEUN0415 <wltnr1998@gamil.com>
2024-03-22 17:50:25 +0900

    about 수정 2024.03.22

--- js/main.js
+++ js/main.js
@@ -109,4 +109,40 @@ $(function () {
         easing: "linear",
         loop: true,
     });
+
+    //scrolling each section
+    window.addEventListener(
+        "wheel",
+        function (e) {
+            e.preventDefault();
+        },
+        { passive: false }
+    );
+
+    let $html = $("html");
+
+    let page = 1;
+
+    let lastPage = $("section").length;
+
+    $html.animate({ scrollTop: 0 }, 10);
+
+    $(window).on("wheel", function(e){
+ 
+        if($html.is(":animated")) return;
+     
+        if(e.originalEvent.deltaY > 0){
+            if(page== lastPage) return;
+     
+            page++;
+        }else if(e.originalEvent.deltaY < 0){
+            if(page == 1) return;
+     
+            page--;
+        }
+        var posTop = (page-1) * $(window).height();
+     
+        $html.animate({scrollTop : posTop});
+     
+    });
 });
