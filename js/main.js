ccbd3dbe107c6eede08d8c328cefb37244793ba1
SEONGEUN0415 <wltnr1998@gamil.com>
2024-03-25 17:43:32 +0900

    fullpage 2024.03.25

--- js/main.js
+++ js/main.js
@@ -1,4 +1,89 @@
 $(function () {
+    //fullpage
+    var myFullpage = new fullpage("#fullpage", {
+        /* sectionsColor: [
+          "#1bbc9b",
+          "#4BBFC3",
+          "#7BAABE",
+          "whitesmoke",
+          "#ccddff",
+        ], */
+        anchors: ["home", "skill", "about", "WORK", "contact"],
+        navigation: true,
+        navigationTooltips: ["HOME", "SKILL", "ABOUT", "WORK", "contact"],
+        showActiveTooltip: true,
+        menu: "#menu",
+    });
+
+    //cursor effect
+    let x1 = 0,
+        y1 = 0;
+    window.client;
+    const vh = Math.max(
+            document.documentElement.clientHeight || 0,
+            window.innerHeight || 0
+        ),
+        dist_to_draw = 50,
+        delay = 1000,
+        fsize = ["1.1rem", "1.4rem", ".8rem", "1.7rem"],
+        colors = [
+            "#E23636",
+            "#F9F3EE",
+            "#E1F8DC",
+            "#B8AFE6",
+            "#AEE1CD",
+            "#5EB0E5",
+        ],
+        rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+        selRand = (o) => o[rand(0, o.length - 1)],
+        distanceTo = (x1, y1, x2, y2) =>
+            Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)),
+        shouldDraw = (x, y) => distanceTo(x1, y1, x, y) >= dist_to_draw,
+        addStr = (x, y) => {
+            const str = document.createElement("div");
+            str.innerHTML = "&#10022;";
+            str.className = "star";
+            str.style.top = `${y + rand(-20, 20)}px`;
+            str.style.left = `${x}px`;
+            str.style.color = selRand(colors);
+            str.style.fontSize = selRand(fsize);
+            document.body.appendChild(str);
+            //console.log(rand(0, 3));
+            const fs = 10 + 5 * parseFloat(getComputedStyle(str).fontSize);
+            //console.log(vh, y, fs);
+            //console.log((y+fs)>vh?vh-y:fs);
+            str.animate(
+                {
+                    translate: `0 ${y + fs > vh ? vh - y : fs}px`,
+                    opacity: 0,
+                    transform: `rotateX(${rand(1, 500)}deg) rotateY(${rand(
+                        1,
+                        500
+                    )}deg)`,
+                },
+                {
+                    duration: delay,
+                    fill: "forwards",
+                }
+            );
+            //could add a animation terminate listener, but why add the additional load
+            setTimeout(() => {
+                str.remove();
+            }, delay);
+        };
+
+    addEventListener("mousemove", (e) => {
+        const { clientX, clientY } = e;
+        if (shouldDraw(clientX, clientY)) {
+            addStr(clientX, clientY);
+            x1 = clientX;
+            y1 = clientY;
+        }
+    });
+
+    //effect02
+
+
     //mainvisual typing
 
     var typingBool = false;
@@ -44,14 +129,14 @@ $(function () {
     } //mainvisual typing end
 
     //skill icon click
-    let skillsDes = document.querySelectorAll(".skills__des p");
-    let skillIcon = document.querySelectorAll(".skills__des--icon li");
+    let skillDes = document.querySelectorAll(".skill__des p");
+    let skillIcon = document.querySelectorAll(".skill__des--icon li");
     for (let i = 0; i < skillIcon.length; i++) {
         skillIcon[i].addEventListener("click", () => {
-            skillsDes.forEach((item) => {
+            skillDes.forEach((item) => {
                 item.classList.remove("on");
             });
-            skillsDes[i].classList.add("on");
+            skillDes[i].classList.add("on");
         });
     } //icon click end
 
@@ -110,58 +195,14 @@ $(function () {
         loop: true,
     });
 
-    //scrolling each section
-    window.addEventListener(
-        "wheel",
-        function (e) {
-            e.preventDefault();
-        },
-        { passive: false }
-    );
-
-    let $html = $("html");
-
-    let page = 1;
-
-    let lastPage = $("section").length;
-
-    $html.animate({ scrollTop: 0 }, 10);
-
-    $(window).on("wheel", function (e) {
-        if ($html.is(":animated")) return;
-
-        if (e.originalEvent.deltaY > 0) {
-            if (page == lastPage) return;
-
-            page++;
-        } else if (e.originalEvent.deltaY < 0) {
-            if (page == 1) return;
-
-            page--;
-        }
-        var posTop = (page - 1) * $(window).height();
-
-        $html.animate({ scrollTop: posTop });
-    });
-
-    let scrollBar = $(window).scrollTop();
-    let work = $(".work");
-    let workTop = $(".work").offset().top;
-    $(window).scroll(function () {});
-
-    // aos library
-    AOS.init();
-
     //graphic design tap menu
-    let designTapMenu = document.querySelectorAll(
-        ".design >ul li button"
-    );
+    let designTapMenu = document.querySelectorAll(".design >ul li button");
     let designList = document.querySelectorAll(".design .list");
 
     for (let i = 0; i < designTapMenu.length; i++) {
         designTapMenu[i].addEventListener("click", () => {
             designList.forEach((item) => {
-                item.classList.remove('on');
+                item.classList.remove("on");
             });
             designList[i].classList.add("on");
         });
